2c338b81038062bf65ad6cb25c18ebe6c6bf311c
David Lora <despinal0425@gmail.com>
2023-03-21 21:07:31 -0400

    getting data from api using article's id

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,19 +1,40 @@
+import Link from 'next/link';
 // To retrieve the id
 import { useRouter } from 'next/router';
 
-const article = () => {
+// pass in the article prop coming from the getServerSideProps()
+const article = ({article}) => {
   // router contains parameters that are in the route ("/article/[id]") or path of the current page
   // get the id number of the route of the current article and assign it to variable id
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const router = useRouter();
-  console.log(router);
-  const { id } = router.query;
+  // const router = useRouter();
+  // console.log(router);
+  // const { id } = router.query;
 
   return (
-    <div>
-      This is an article {id}
-    </div>
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go back</Link>
+    </>
   )
 }
+
+// fetch data at the time of request rather than get static props at build time
+// pass in the context parameter to the getServerSideProps() which allows to get the id thats on the url
+// make fetch req to the api and assign the api response to the 'article' variable
+export const getServerSideProps = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article
+    }
+  }
+}
+
 
 export default article;
